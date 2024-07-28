86bcd299f9a94f699091a4756275e3f5641f1776
AllanVargens <allanvargens@gmail.com>
2024-07-28 14:12:44 -0300

    feat: inital structure

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.scss',
+})
+export class DefaultLoginLayoutComponent {}
